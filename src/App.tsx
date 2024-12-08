87d6dc78071dded4d26161ec9a1d93487ac095cd
Swag-blips <coderblip@gmail.com>
2024-12-08 17:49:40 +0100

    feat:sidebar created

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import Chats from "./components/Chats";
+import Sidebar from "./components/Sidebar";
+
 function App() {
-  return <></>;
+  return (
+    <div className="flex">
+      <Sidebar />
+      <Chats />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,33 @@
+import logo from "/assets/logo.svg";
+import { GoHomeFill } from "react-icons/go";
+import { BsChatText } from "react-icons/bs";
+import { CgProfile } from "react-icons/cg";
+import { IoSettingsOutline } from "react-icons/io5";
+import profilePic from "/assets/profile.png";
+
+const Sidebar = () => {
+  return (
+    <aside className="bg-[#12171D] py-6 flex justify-center w-[8.8888%] h-screen">
+      <div className="flex flex-col justify-between items-center">
+        <div className="flex flex-col items-center gap-12">
+          <figure>
+            <img src={logo} alt="logo" />
+          </figure>
+
+          <div className="flex flex-col items-center gap-8">
+            <GoHomeFill size={30} className="text-white" />
+            <BsChatText size={24} className="text-white" />
+            <CgProfile size={24} className="text-white" />
+            <IoSettingsOutline size={24} className="text-white" />
+          </div>
+        </div>
+
+        <figure>
+          <img src={profilePic} alt="profile-pic" />
+        </figure>
+      </div>
+    </aside>
+  );
+};
+
+export default Sidebar;
