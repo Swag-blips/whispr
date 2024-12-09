32f71d3ba08f6b3b1a514e212d3aca34b7563d91
Swag-blips <coderblip@gmail.com>
2024-12-09 15:38:33 +0100

    feat:select chat implemented

--- src/components/SelectChat.tsx
+++ src/components/SelectChat.tsx
@@ -1,7 +1,14 @@
-
+import noChat from "/assets/no-chat.png";
 
 const SelectChat = () => {
-  return <div>SelectChat</div>;
+  return (
+    <main className="flex items-center flex-col w-[67.22222%] justify-center">
+      <img src={noChat} alt="no-chat illustration" />
+      <h2 className="text-[32px] text-white font-medium">
+        Select a message and chat
+      </h2>
+    </main>
+  );
 };
 
 export default SelectChat;
